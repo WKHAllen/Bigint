4e55444db9afcfc6b575ad64e02ebe7beb112472
WKHAllen <wkhallen@gmail.com>
2020-12-19 13:57:50 -0600

    Add test file

---
+++ test/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <assert.h>
+#include "../src/bigint.h"
+
+int main(int argc, char **argv)
+{
+	printf("Running tests...");
+
+
+
+	printf("Tests completed successfully.");
+}
